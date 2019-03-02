8bd8e67353015c79dbbc466ffd908dc995b9b155
it-zyk <it-zyk@outlook.com>
2019-03-03 00:05:14 +0800

    PythonCode

--- 17_WSGI-mini-web框架/09_mini_web操作mysql数据库/dynamic/mini_frame.py
+++ 17_WSGI-mini-web框架/09_mini_web操作mysql数据库/dynamic/mini_frame.py
@@ -178,6 +178,51 @@ def del_focus(ret):
     return "取消关注成功"
 
 
+@route(r"/update/(\d+)\.html")
+def show_update_page(ret):
+    """显示修改的那个页面"""
+    # 1. 获取股票代码
+    stock_code = ret.group(1)
+
+    # 2. 打开模板
+    with open("./templates/update.html") as f:
+        content = f.read()
+
+    # 3. 根据股票代码查询相关的备注信息
+    db = pymysql.connect(host='localhost', port=3306, user='root',
+                         password='mysql', database='stock_db', charset='utf8')
+    cs = conn.cursor()
+    sql = """select f.note_info from focus as f inner join info as i on i.id=f.info_id where i.code=%s;"""
+    cs.execute(sql, (stock_code,))
+    stock_infos = cs.fetchone()
+    note_info = stock_infos[0]  # 获取这个股票对应的备注信息
+    cs.close()
+    db.close()
+
+    content = re.sub(r"\{%note_info%\}", note_info, content)
+    content = re.sub(r"\{%code%\}", stock_code, content)
+
+    return content
+
+
+@route(r"/update/(\d+)/(.*)\.html")
+def save_update_page(ret):
+    """"保存修改的信息"""
+    stock_code = ret.group(1)
+    comment = ret.group(2)
+
+    db = pymysql.connect(host='localhost', port=3306, user='root',
+                         password='mysql', database='stock_db', charset='utf8')
+    cs = conn.cursor()
+    sql = """update focus set note_info=%s where info_id = (select id from info where code=%s);"""
+    cs.execute(sql, (comment, stock_code))
+    db.commit()
+    cs.close()
+    db.close()
+
+    return "修改成功..."
+
+
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
 
