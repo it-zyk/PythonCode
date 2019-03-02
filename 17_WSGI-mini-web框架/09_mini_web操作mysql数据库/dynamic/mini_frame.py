86baf9c73cea6204e373f6d8577b953d055ecd1b
it-zyk <it-zyk@outlook.com>
2019-03-02 17:39:43 +0800

    PythonCode

--- 17_WSGI-mini-web框架/09_mini_web操作mysql数据库/dynamic/mini_frame.py
+++ 17_WSGI-mini-web框架/09_mini_web操作mysql数据库/dynamic/mini_frame.py
@@ -5,10 +5,12 @@ import pymysql
 
 URL_FUNC_DICT = dict()
 
+
 def route(url):
     def set_func(func):
         # URL_FUNC_DICT["/index.py"] = index
-        URL_FUNC_DICT[url] = func 
+        URL_FUNC_DICT[url] = func
+
         def call_func(*args, **kwargs):
             return func(*args, **kwargs)
         return call_func
@@ -23,7 +25,8 @@ def index(ret):
     # my_stock_info = "哈哈哈哈 这是你的本月名称....."
 
     # content = re.sub(r"\{%content%\}", my_stock_info, content)
-    db = pymysql.connect(host='localhost',port=3306,user='root',password='root',database='stock_db',charset='utf8')
+    db = pymysql.connect(host='localhost', port=3306, user='root',
+                         password='root', database='stock_db', charset='utf8')
     cursor = db.cursor()
     sql = """select * from info;"""
     cursor.execute(sql)
@@ -31,8 +34,7 @@ def index(ret):
     cursor.close()
     db.close()
 
-
-    tr_tempt= """
+    tr_tempt = """
     <tr>
     <td>%s</td>
     <td>%s</td>
@@ -50,17 +52,19 @@ def index(ret):
     html = ""
 
     for line_info in stock_infos:
-        html += tr_tempt % (line_info[0], line_info[1], line_info[2], line_info[3], line_info[4], line_info[5], line_info[6], line_info[7], line_info[1])
+        html += tr_tempt % (line_info[0], line_info[1], line_info[2], line_info[3],
+                            line_info[4], line_info[5], line_info[6], line_info[7], line_info[1])
     content = re.sub(r"\{%content%\}", html, content)
     return content
-     
-@route("/center.html") 
+
+
+@route("/center.html")
 def center(ret):
     with open("./templates/center.html") as f:
         content = f.read()
 
-
-    db = pymysql.connect(host='localhost',port=3306,user='root',password='root',database='stock_db',charset='utf8')
+    db = pymysql.connect(host='localhost', port=3306, user='root',
+                         password='root', database='stock_db', charset='utf8')
     cursor = db.cursor()
     sql = """select a.code,a.short,a.chg,a.turnover,a.price,a.highs, b.note_info from info as a inner join focus as b on a.id = b.id; """
     cursor.execute(sql)
@@ -68,8 +72,7 @@ def center(ret):
     cursor.close()
     db.close()
 
-
-    tr_tempt= """
+    tr_tempt = """
     <tr>
         <td>%s</td>
         <td>%s</td>
@@ -86,26 +89,49 @@ def center(ret):
     html = ""
 
     for line_info in stock_infos:
-        html += tr_tempt % (line_info[0], line_info[1], line_info[2], line_info[3], line_info[4], line_info[5], line_info[6])
+        html += tr_tempt % (line_info[0], line_info[1], line_info[2],
+                            line_info[3], line_info[4], line_info[5], line_info[6])
     content = re.sub(r"\{%content%\}", html, content)
     return content
-     
+
  #
- #URL_FUNC_DICT = {
+ # URL_FUNC_DICT = {
  #        "/index.py": index,
  #        "/center.py": center
  #        }
  #
 
-@route("/add/\d+\.html")
+
+@route(r"/add/(\d+)\.html")
 def add_focus(ret):
+    # 1.获取股票代码
+    # 2. 判断试下是否有这个股票代码，防止恶意添加
+    # 3. 判断以下是否已经关注过
+
     stock_code = ret.group(1)
-    return "add focus %s" % str(stock_code)
+
+    db = pymysql.connect(host='localhost', port=3306, user='root',
+                         password='root', database='stock_db', charset='utf8')
+    cursor = db.cursor()
+    sql = """selct * from info as i  inner join focus as f on i.id=f.info_id where i.code = %s; """
+    cursor.execute(sql, (stock_code,))
+    # 如果要是没有这个股票代码，那么认为是非法的请求
+    if not cursor.fetchone():
+        cursor.close()
+        db.close()
+        retun "已经关注了，请勿重复关注..."
+    # 4. 添加关注
+    sql ="""insert into focus (info_id) select id from info where code = %s """
+    cursor.execute(sql, stock_code)
+    db.commit()
+    cursor.close()
+    db.close()
+    return "add %s OK "  % str(stock_code)
 
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
-    
+
     file_name = env['PATH_INFO']
     # file_name = "/index.py"
 
@@ -115,10 +141,8 @@ def application(env, start_response):
             ret = re.match(url, file_name)
             if ret:
                 return func(ret)
-        
+
         # func = URL_FUNC_DICT[file_name]
         # return func()
     except Exception as ret:
         return "产生了异常 %s" % str(ret)
-    
-
