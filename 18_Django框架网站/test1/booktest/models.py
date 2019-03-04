c30a59093907b91e433cdea1810406b02f71c0bb
it-zyk <it-zyk@outlook.com>
2019-03-04 14:16:14 +0800

    PythonCode

--- 18_Django框架网站/test1/booktest/models.py
+++ 18_Django框架网站/test1/booktest/models.py
@@ -8,4 +8,15 @@ class BookInfo(models.Model):
     # max_length 指定字符串的长度
     btitle = models.CharField(max_length=20)
     # 出版日期 DataField 日期类型
-    bpub_date = models.DataField()
+    bpub_date = models.DateField()
+
+
+class HeroInfo(models.Model):
+    """英雄人物模型类"""
+    hnmae = models.CharField(max_length=20)   #英雄名称
+    # Fase 默认值 代表男性
+    hgender = models.BooleanField(default=False)
+    # 备注 
+    hcomment = models.CharField(max_length=128)
+    # 关系属性 建立图书类和英雄人物类之间的一对多关系
+    hbook = models.ForeignKey('BookInfo')
