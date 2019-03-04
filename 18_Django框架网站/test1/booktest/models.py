0944ac599576c89ce2d4957a894074f0e8f8aa50
it-zyk <it-zyk@outlook.com>
2019-03-04 09:45:02 +0800

    PythonCode

--- 18_Django框架网站/test1/booktest/models.py
+++ 18_Django框架网站/test1/booktest/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BookInfo(models.Model):
+    """图书模型类"""
+    # max_length 指定字符串的长度
+    btitle = models.CharField(max_length=20)
+    # 出版日期 DataField 日期类型
+    bpub_date = models.DataField()
