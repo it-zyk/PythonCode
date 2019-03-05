03c086f44217025d29c2689b9a0971a4c8cd8ddd
it-zyk <it-zyk@outlook.com>
2019-03-05 14:06:11 +0800

    PythonCode

--- 18_Django框架网站/test1/booktest/views.py
+++ 18_Django框架网站/test1/booktest/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader, RequestContext
 
+from booktest.models import BookInfo  # 导入图书类
 # Create your views here.
 
 
@@ -13,6 +14,8 @@ def my_render(request, temp_path, context_dict={}):
     return HttpResponse(res_html)
 
     # http://localhost:8000/index
+
+
 def index(request):
     # 进行处理， 和M和T进行交互
     # return HttpResponse('老铁没毛病')
@@ -21,7 +24,22 @@ def index(request):
 
     # return my_render(request, 'booktest/index.html')
 
-    return render(request, 'booktest/index.html', {'content' : 'Hello world!', 'list': list(range(1, 10))})
+    return render(request, 'booktest/index.html', {'content': 'Hello world!', 'list': list(range(1, 10))})
 
 
-    
+def show_books(request):
+    '''显示图书的信息'''
+    # 1. 通过M查图片表的数据
+    books = BookInfo.objects.all()
+
+    return render(request, 'booktest/show_books.html', {'book': books})
+
+
+def detail(request, bid):
+    '''查询图书关联英雄信息'''
+    # 1. 根据bid 查询图书信息
+    book = BookInfo.objects.get(id=bid)
+    # 2 查询和book关联的英雄信息
+    heros = book.heroinfo_set.all()
+    # 3. 使用模板
+    return render(request, 'booktest/detail.html', {'book': book, 'heros': heros})
