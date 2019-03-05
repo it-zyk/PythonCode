50bfda3859e813e7178ca321d0adf6f8f7a2634d
it-zyk <it-zyk@outlook.com>
2019-03-05 21:38:35 +0800

    Django mysql

--- 18_Django框架网站/test2/booktest/views.py
+++ 18_Django框架网站/test2/booktest/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from booktest.models import BookInfo
 # Create your views here.
+
+
+
+
+def index(request):
+    '''显示图书信息'''
+    # 1. 查询所有图书信息
+    books = BookInfo.objects.all()
+    # 2 使用模板
+    render(request,'templates/index.html', {'books':books})
