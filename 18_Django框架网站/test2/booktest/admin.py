4d43e89e3095a385351c975741a0ae357b6f5003
it-zyk <it-zyk@outlook.com>
2019-03-05 22:12:01 +0800

    Django mysql

--- 18_Django框架网站/test2/booktest/admin.py
+++ 18_Django框架网站/test2/booktest/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from booktest.models import BookInfo, HeroInfo
+# Register your models here.
+
+
+class BookInfoAdmin(admin.ModelAdmin):
+    """图书模型管理类"""
+    list_display = ['id', 'btitle', 'bpub_date', 'bread', 'bcomment']
+
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    """图书模型管理类"""
+    list_display = ['id', 'hname', 'hcomment']
+
 
 # Register your models here.
+# 注册模型类
+admin.site.register(BookInfo, BookInfoAdmin)
+admin.site.register(HeroInfo, HeroInfoAdmin)
--- 18_Django框架网站/test2/booktest/views.py
+++ 18_Django框架网站/test2/booktest/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from booktest.models import BookInfo
+from datetime import date
+from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
-
-
 
 
 def index(request):
@@ -10,4 +10,15 @@ def index(request):
     # 1. 查询所有图书信息
     books = BookInfo.objects.all()
     # 2 使用模板
-    render(request,'templates/index.html', {'books':books})
+    return render(request, 'booktest/index.html', {'books': books})
+
+
+def create(request):
+    '''新增信息'''
+    b = BookInfo()
+    b.btitle = '新流星蝴蝶剑'
+    b.bpub_date = date(1990, 1, 1)
+    b.save()
+    # 让浏览器再访问首页
+    # return HttpResponse('OK')
+    return HttpResponseRedirect('/index')
