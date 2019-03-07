755f5bd245067459aebeb3f3de5c8459d302a689
it-zyk <it-zyk@outlook.com>
2019-03-07 16:03:15 +0800

    Django View

--- 18_Django框架网站/test3/booktest/urls.py
+++ 18_Django框架网站/test3/booktest/urls.py
@@ -6,4 +6,8 @@ from booktest import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^index$', views.index),
+    # 捕获位置参数
+    # url(r'^showarg(\d+)$', views.show_arg)
+    # 捕获关键字参数
+    url(r'^showarg(?P<num>\d+)$', views.show_arg),
 ]
