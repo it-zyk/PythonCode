3a3fa93da7a277478e6d8e27fce1979e6ffb289d
it-zyk <it-zyk@outlook.com>
2019-03-20 15:50:15 +0800

    Django

--- 18_Django框架网站/dailyfresh/apps/user/urls.py
+++ 18_Django框架网站/dailyfresh/apps/user/urls.py
@@ -1,6 +1,8 @@
 
 
 from django.conf.urls import include, url
+from user import views
 
 urlpatterns = [
+    url(r'^register$', include(admin.site.urls), name='register'), #注册
 ]
--- 18_Django框架网站/dailyfresh/apps/user/views.py
+++ 18_Django框架网站/dailyfresh/apps/user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def register(request):
+    return render(request, 'user/register.html')
