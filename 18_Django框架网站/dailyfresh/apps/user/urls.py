08c86d8acad4354a81c1acc8f34f782e43a68975
it-zyk <it-zyk@outlook.com>
2019-03-20 15:54:21 +0800

    Django

--- 18_Django框架网站/dailyfresh/apps/user/urls.py
+++ 18_Django框架网站/dailyfresh/apps/user/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import include, url
 from user import views
 
 urlpatterns = [
-    url(r'^register$', include(admin.site.urls), name='register'), #注册
+    url(r'^register$', views.register, name='register'),  # 注册
 ]
