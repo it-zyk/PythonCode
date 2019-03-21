6791041065a7e73004f1607c0108e8911f00005e
it-zyk <it-zyk@outlook.com>
2019-03-21 11:52:51 +0800

    Django

--- 18_Django框架网站/dailyfresh/apps/user/urls.py
+++ 18_Django框架网站/dailyfresh/apps/user/urls.py
@@ -2,8 +2,11 @@
 
 from django.conf.urls import include, url
 from user import views
+from user.views import RegisterView, LoginView
 
 urlpatterns = [
-    url(r'^register$', views.register, name='register'),  # 注册
-    url(r'^register_handle$', views.register_handle, name='register_handle'),  # 注册处理
+    # url(r'^register$', views.register, name='register'),  # 注册
+    # url(r'^register_handle$', views.register_handle, name='register_handle'),  # 注册处理
+    url(r'^rgeister$', RegisterView.as_view(), name='register'),
+    url(r'^login$', LoginView.as_view(), name='login'),
 ]
--- 18_Django框架网站/dailyfresh/apps/user/views.py
+++ 18_Django框架网站/dailyfresh/apps/user/views.py
@@ -2,9 +2,63 @@ from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 import re
 from user.models import User
+from django.views.generic import View
 # Create your views here.
 
 
+class RegisterView(View):
+    '''注册类视图'''
+
+    def get(self, request):
+        '''显示注册页面'''
+        return render(request, 'user/register.html')
+
+    def post(self, request):
+        '''进行注册处理'''
+        # 1.接收数据
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+        if allow != 'on':
+            return render(request, 'user/register.html', {'errmsg': '请同意协议'})
+        # 2. 进行数据校验
+        if not all([username, password, email]):
+            # 数据不完整
+            return render(request, 'user/register.html', {'errmsg': '注册信息不完整'})
+        # 校验邮箱
+        if not re.match(r'^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'user/register.html', {'errmsg': '邮箱格式不合法'})
+
+        # 3. 进行业务处理： 进行用户注册
+        # 校验用户名是否重复
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            # 用户名不存在
+            user = None
+
+        if user:
+            return render(request, 'user/register.html', {'errmsg': '用户名已存在'})
+
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        user.save()
+        # 4，返回应答 跳转首页
+        return redirect(reverse('goods:index'))
+
+
+class LoginView(View):
+    '''登录页面'''
+
+    def get(self, request):
+        return render(request, 'user/login.html')
+
+    def post(self, request):
+        pass
+
+
+# GET POST PUT DELETE OPTION
 def register(request):
     if request.method == 'GET':
         return render(request, 'user/register.html')
