b11b5537a2cceb275bdc15679631f13ff6ae93ad
it-zyk <it-zyk@outlook.com>
2019-03-22 17:23:51 +0800

    Django

--- 18_Django框架网站/dailyfresh/apps/user/urls.py
+++ 18_Django框架网站/dailyfresh/apps/user/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     url(r'^$', login_required(UserInfoView.as_view()), name='user'),  # 用户中心-信息页
 
     # url(r'^$', UserInfoView.as_view(), name='user'),  # 用户中心-首页
-    url(r'^order$', UserOrderView.as_view(), name='order'),  # 用户首页-订单
+    url(r'^order$', login_required(UserOrderView.as_view()), name='order'),  # 用户首页-订单
     url(r'^address$', AddressView.as_view(), name='address'),  # 用户中心地址
 
 ]
