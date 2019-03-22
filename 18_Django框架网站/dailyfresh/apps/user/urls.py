4f42b6ad8a2641bafbaf89dc878eab820b3a608c
it-zyk <it-zyk@outlook.com>
2019-03-22 09:42:33 +0800

    Django

--- 18_Django框架网站/dailyfresh/apps/user/urls.py
+++ 18_Django框架网站/dailyfresh/apps/user/urls.py
@@ -7,7 +7,7 @@ from user.views import RegisterView, LoginView,ActiveView
 urlpatterns = [
     # url(r'^register$', views.register, name='register'),  # 注册
     # url(r'^register_handle$', views.register_handle, name='register_handle'),  # 注册处理
-    url(r'^rgeister$', RegisterView.as_view(), name='register'),
+    url(r'^register$', RegisterView.as_view(), name='register'),
     url(r'^login$', LoginView.as_view(), name='login'),
     url(r'^active/(?P<token>.*)$', ActiveView.as_view(), name='active'), # 用户激活
 
