9a8205c5c1926bf4dcd240fb491c44ae9786951b
it-zyk <it-zyk@outlook.com>
2019-04-04 16:08:38 +0800

    Django

--- 18_Django框架网站/dailyfresh/apps/user/views.py
+++ 18_Django框架网站/dailyfresh/apps/user/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render,redirect
 from django.core.urlresolvers import reverse
 from django.core.mail import send_mail
 from django.contrib.auth import authenticate, login, logout
+from django.core.paginator import Paginator
 from django.views.generic import View
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.conf import settings
 
 from user.models import User, Address
 from goods.models import GoodsSKU
+from order.models import OrderInfo,OrderGoods
+
 from celery_tasks.tasks import send_register_active_email
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import SignatureExpired
@@ -76,14 +79,14 @@ def register_handle(request):
     # 进行数据校验
     if not all([username, password, email]):
         # 数据不完整
-        return render(request, 'register.html', {'errmsg': '数据不完整'})
+        return render(request, 'register.html', {'errmsg':'数据不完整'})
 
     # 校验邮箱
     if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
-        return render(request, 'register.html', {'errmsg': '邮箱格式不正确'})
+        return render(request, 'register.html', {'errmsg':'邮箱格式不正确'})
 
     if allow != 'on':
-        return render(request, 'register.html', {'errmsg': '请同意协议'})
+        return render(request, 'register.html', {'errmsg':'请同意协议'})
 
     # 校验用户名是否重复
     try:
@@ -94,7 +97,7 @@ def register_handle(request):
 
     if user:
         # 用户名已存在
-        return render(request, 'register.html', {'errmsg': '用户名已存在'})
+        return render(request, 'register.html', {'errmsg':'用户名已存在'})
 
     # 进行业务处理: 进行用户注册
     user = User.objects.create_user(username, email, password)
@@ -108,7 +111,6 @@ def register_handle(request):
 # /user/register
 class RegisterView(View):
     '''注册'''
-
     def get(self, request):
         '''显示注册页面'''
         return render(request, 'register.html')
@@ -154,8 +156,8 @@ class RegisterView(View):
 
         # 加密用户的身份信息，生成激活token
         serializer = Serializer(settings.SECRET_KEY, 3600)
-        info = {'confirm': user.id}
-        token = serializer.dumps(info)  # bytes
+        info = {'confirm':user.id}
+        token = serializer.dumps(info) # bytes
         token = token.decode()
 
         # 发邮件
@@ -167,7 +169,6 @@ class RegisterView(View):
 
 class ActiveView(View):
     '''用户激活'''
-
     def get(self, request, token):
         '''进行用户激活'''
         # 进行解密，获取要激活的用户信息
@@ -192,7 +193,6 @@ class ActiveView(View):
 # /user/login
 class LoginView(View):
     '''登录'''
-
     def get(self, request):
         '''显示登录页面'''
         # 判断是否记住了用户名
@@ -204,7 +204,7 @@ class LoginView(View):
             checked = ''
 
         # 使用模板
-        return render(request, 'login.html', {'username': username, 'checked': checked})
+        return render(request, 'login.html', {'username':username, 'checked':checked})
 
     def post(self, request):
         '''登录校验'''
@@ -214,7 +214,7 @@ class LoginView(View):
 
         # 校验数据
         if not all([username, password]):
-            return render(request, 'login.html', {'errmsg': '数据不完整'})
+            return render(request, 'login.html', {'errmsg':'数据不完整'})
 
         # 业务处理:登录校验
         user = authenticate(username=username, password=password)
@@ -230,15 +230,14 @@ class LoginView(View):
                 next_url = request.GET.get('next', reverse('goods:index'))
 
                 # 跳转到next_url
-                response = redirect(next_url)  # HttpResponseRedirect
+                response = redirect(next_url) # HttpResponseRedirect
 
                 # 判断是否需要记住用户名
                 remember = request.POST.get('remember')
 
                 if remember == 'on':
                     # 记住用户名
-                    response.set_cookie(
-                        'username', username, max_age=7 * 24 * 3600)
+                    response.set_cookie('username', username, max_age=7*24*3600)
                 else:
                     response.delete_cookie('username')
 
@@ -246,16 +245,15 @@ class LoginView(View):
                 return response
             else:
                 # 用户未激活
-                return render(request, 'login.html', {'errmsg': '账户未激活'})
+                return render(request, 'login.html', {'errmsg':'账户未激活'})
         else:
             # 用户名或密码错误
-            return render(request, 'login.html', {'errmsg': '用户名或密码错误'})
+            return render(request, 'login.html', {'errmsg':'用户名或密码错误'})
 
 
 # /user/logout
 class LogoutView(View):
     '''退出登录'''
-
     def get(self, request):
         '''退出登录'''
         # 清除用户的session信息
@@ -268,7 +266,6 @@ class LogoutView(View):
 # /user
 class UserInfoView(LoginRequiredMixin, View):
     '''用户中心-信息页'''
-
     def get(self, request):
         '''显示'''
         # Django会给request对象添加一个属性request.user
@@ -285,10 +282,10 @@ class UserInfoView(LoginRequiredMixin, View):
         # sr = StrictRedis(host='172.16.179.130', port='6379', db=9)
         con = get_redis_connection('default')
 
-        history_key = 'history_%d' % user.id
+        history_key = 'history_%d'%user.id
 
         # 获取用户最新浏览的5个商品的id
-        sku_ids = con.lrange(history_key, 0, 4)  # [2,3,1]
+        sku_ids = con.lrange(history_key, 0, 4) # [2,3,1]
 
         # 从数据库中查询用户浏览的商品的具体信息
         # goods_li = GoodsSKU.objects.filter(id__in=sku_ids)
@@ -306,9 +303,9 @@ class UserInfoView(LoginRequiredMixin, View):
             goods_li.append(goods)
 
         # 组织上下文
-        context = {'page': 'user',
-                   'address': address,
-                   'goods_li': goods_li}
+        context = {'page':'user',
+                   'address':address,
+                   'goods_li':goods_li}
 
         # 除了你给模板文件传递的模板变量之外，django框架会把request.user也传给模板文件
         return render(request, 'user_center_info.html', context)
@@ -317,33 +314,86 @@ class UserInfoView(LoginRequiredMixin, View):
 # /user/order
 class UserOrderView(LoginRequiredMixin, View):
     '''用户中心-订单页'''
-
-    def get(self, request):
+    def get(self, request, page):
         '''显示'''
         # 获取用户的订单信息
+        user = request.user
+        orders = OrderInfo.objects.filter(user=user).order_by('-create_time')
 
-        return render(request, 'user_center_order.html', {'page': 'order'})
+        # 遍历获取订单商品的信息
+        for order in orders:
+            # 根据order_id查询订单商品信息
+            order_skus = OrderGoods.objects.filter(order_id=order.order_id)
+
+            # 遍历order_skus计算商品的小计
+            for order_sku in order_skus:
+                # 计算小计
+                amount = order_sku.count*order_sku.price
+                # 动态给order_sku增加属性amount,保存订单商品的小计
+                order_sku.amount = amount
+
+            # 动态给order增加属性，保存订单状态标题
+            order.status_name = OrderInfo.ORDER_STATUS[order.order_status]
+            # 动态给order增加属性，保存订单商品的信息
+            order.order_skus = order_skus
+
+        # 分页
+        paginator = Paginator(orders, 1)
+
+        # 获取第page页的内容
+        try:
+            page = int(page)
+        except Exception as e:
+            page = 1
+
+        if page > paginator.num_pages:
+            page = 1
+
+        # 获取第page页的Page实例对象
+        order_page = paginator.page(page)
+
+        # todo: 进行页码的控制，页面上最多显示5个页码
+        # 1.总页数小于5页，页面上显示所有页码
+        # 2.如果当前页是前3页，显示1-5页
+        # 3.如果当前页是后3页，显示后5页
+        # 4.其他情况，显示当前页的前2页，当前页，当前页的后2页
+        num_pages = paginator.num_pages
+        if num_pages < 5:
+            pages = range(1, num_pages + 1)
+        elif page <= 3:
+            pages = range(1, 6)
+        elif num_pages - page <= 2:
+            pages = range(num_pages - 4, num_pages + 1)
+        else:
+            pages = range(page - 2, page + 3)
+
+        # 组织上下文
+        context = {'order_page':order_page,
+                   'pages':pages,
+                   'page': 'order'}
+
+        # 使用模板
+        return render(request, 'user_center_order.html', context)
 
 
 # /user/address
 class AddressView(LoginRequiredMixin, View):
     '''用户中心-地址页'''
-
     def get(self, request):
         '''显示'''
         # 获取登录用户对应User对象
         user = request.user
 
         # 获取用户的默认收货地址
-        # try:
-        #     address = Address.objects.get(user=user, is_default=True) # models.Manager
-        # except Address.DoesNotExist:
-        #     # 不存在默认收货地址
-        #     address = None
-        address = Address.objects.get_default_address(user)
+        try:
+            address = Address.objects.get(user=user, is_default=True) # models.Manager
+        except Address.DoesNotExist:
+            # 不存在默认收货地址
+            address = None
+        # address = Address.objects.get_default_address(user)
 
         # 使用模板
-        return render(request, 'user_center_site.html', {'page': 'address', 'address': address})
+        return render(request, 'user_center_site.html', {'page':'address', 'address':address})
 
     def post(self, request):
         '''地址的添加'''
@@ -354,12 +404,12 @@ class AddressView(LoginRequiredMixin, View):
         phone = request.POST.get('phone')
 
         # 校验数据
-        if not all([receiver, addr, phone]):
-            return render(request, 'user_center_site.html', {'errmsg': '数据不完整'})
+        if not all([receiver, addr, phone, type]):
+            return render(request, 'user_center_site.html', {'errmsg':'数据不完整'})
 
         # 校验手机号
         if not re.match(r'^1[3|4|5|7|8][0-9]{9}$', phone):
-            return render(request, 'user_center_site.html', {'errmsg': '手机格式不正确'})
+            return render(request, 'user_center_site.html', {'errmsg':'手机格式不正确'})
 
         # 业务处理：地址添加
         # 如果用户已存在默认收货地址，添加的地址不作为默认收货地址，否则作为默认收货地址
@@ -388,4 +438,4 @@ class AddressView(LoginRequiredMixin, View):
                                is_default=is_default)
 
         # 返回应答,刷新地址页面
-        return redirect(reverse('user:address'))  # get请求方式
+        return redirect(reverse('user:address')) # get请求方式
