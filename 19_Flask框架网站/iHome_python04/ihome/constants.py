5338f3c3d0bb31439fadacd9ec729282340346dc
it-zyk <it-zyk@outlook.com>
2019-04-30 14:31:53 +0800

    PythonCode

--- 19_Flask框架网站/iHome_python04/ihome/constants.py
+++ 19_Flask框架网站/iHome_python04/ihome/constants.py
@@ -14,3 +14,9 @@ LOGIN_ERROR_MAX_TIMES = 5
 
 # 登录错误限制的时间, 单位：秒
 LOGIN_ERROR_FORBID_TIME = 600
+
+# 设置fdfs存储服务器上nginx的IP和端口号
+FDFS_URL = 'http://10.2.90.78:8888/'
+
+# 设置fdfs使用的client.conf文件路径
+FDFS_CLIENT_CONF = './utils/client.conf'
