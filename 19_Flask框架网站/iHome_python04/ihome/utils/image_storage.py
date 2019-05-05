a47fa201a9eb61c12c643b3cf47e830192364e93
it-zyk <it-zyk@outlook.com>
2019-05-05 11:26:57 +0800

    PythonCode

--- 19_Flask框架网站/iHome_python04/ihome/utils/image_storage.py
+++ 19_Flask框架网站/iHome_python04/ihome/utils/image_storage.py
@@ -16,6 +16,7 @@ class FDFSStorage(object):
 
     def storage(self, file_data):
         # 创建一个Fdfs_client对象
+        print(self.client_conf)
         client = Fdfs_client(self.client_conf)
 
         # 上传文件到fast dfs系统中
